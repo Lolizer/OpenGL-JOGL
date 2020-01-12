cab9c3cece1961696c20ac9dd9ca17289d3697a2
Lolizer <ardnya@rambler.ru>
2020-01-12 07:35:42 +0300

    planetary

--- src/main/java/com/mulaev/ardnya/Game/Main.java
+++ src/main/java/com/mulaev/ardnya/Game/Main.java
@@ -11,15 +11,18 @@ import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.FPSAnimator;
 import graphicslib3D.GLSLUtils;
 import graphicslib3D.Matrix3D;
+import graphicslib3D.MatrixStack;
 
 import javax.swing.*;
 
+import java.awt.*;
 import java.nio.FloatBuffer;
 
 import static com.jogamp.opengl.GL.GL_ARRAY_BUFFER;
 import static com.jogamp.opengl.GL.GL_DEPTH_BUFFER_BIT;
 import static com.jogamp.opengl.GL.GL_DEPTH_TEST;
 import static com.jogamp.opengl.GL.GL_FLOAT;
+import static com.jogamp.opengl.GL.GL_FRONT_AND_BACK;
 import static com.jogamp.opengl.GL.GL_LEQUAL;
 import static com.jogamp.opengl.GL.GL_STATIC_DRAW;
 import static com.jogamp.opengl.GL.GL_TRIANGLES;
@@ -29,6 +32,7 @@ import static com.jogamp.opengl.GL2ES2.GL_LINK_STATUS;
 import static com.jogamp.opengl.GL2ES2.GL_VERTEX_SHADER;
 
 import static com.jogamp.opengl.GL2ES3.GL_COLOR;
+import static com.jogamp.opengl.GL2GL3.GL_LINE;
 import static graphicslib3D.GLSLUtils.checkOpenGLError;
 import static graphicslib3D.GLSLUtils.printProgramLog;
 import static graphicslib3D.GLSLUtils.printShaderLog;
@@ -37,13 +41,17 @@ import static graphicslib3D.GLSLUtils.readShaderSource;
 
 public class Main extends JFrame implements GLEventListener {
     private GLCanvas myCanvas;
+    private Matrix3D pMat;
+    private MatrixStack scene;
     private int rendering_program;
     private int vao[] = new int[1];
     private int vbo[ ] = new int[2];
     private float cameraX, cameraY, cameraZ;
-    private float cubeLocX, cubeLocY, cubeLocZ;
-    private GLSLUtils util = new GLSLUtils();
-    private Matrix3D pMat;
+    private float sunX, sunY, sunZ;
+    private float earthX, earthY, earthZ;
+    private float moonX, moonY, moonZ;
+
+    private GLSLUtils util;
     private FPSAnimator animator;
 
     public Main(){
@@ -53,6 +61,7 @@ public class Main extends JFrame implements GLEventListener {
 
         setTitle("Game");
         setSize(800, 600);
+        setMinimumSize(getSize());
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         myCanvas = new GLCanvas(caps);
@@ -70,13 +79,16 @@ public class Main extends JFrame implements GLEventListener {
 
     public void init(GLAutoDrawable glAutoDrawable) {
         GL4 gl = (GL4) GLContext.getCurrentGL();
-
+        util = new GLSLUtils();
         rendering_program = createShaderProgram(glAutoDrawable);
         setupVertices();
-        cameraX = 0.0f; cameraY = 0.0f; cameraZ = 1000.0f;
-        cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = -4.0f;
+        cameraX = 0.0f; cameraY = 0.0f; cameraZ = 40.0f;
+        sunX = 0.0f; sunY = 0.0f; sunZ = 0.0f;
+        earthX = 8.0f; earthY = 0.0f; earthZ = 0.0f;
+        moonX = 2.0f; moonY = 4.0f; moonZ = 0.0f;
         float aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
-        pMat = perspective(60.0f, aspect, 0.1f, 1000.0f);
+        pMat = perspective(25.0f, aspect, 1.0f, 1000.0f);
+        scene = new MatrixStack(20);
     }
 
     public void dispose(GLAutoDrawable glAutoDrawable) {
@@ -85,7 +97,9 @@ public class Main extends JFrame implements GLEventListener {
 
     public void display(GLAutoDrawable glAutoDrawable) {
         GL4 gl = (GL4) GLContext.getCurrentGL();
-        double t = (double) (System.currentTimeMillis() % 3600000) / 10000.0;
+        int proj_loc = gl.glGetUniformLocation(rendering_program, "proj_matrix");
+        int mv_loc = gl.glGetUniformLocation(rendering_program, "mv_matrix");
+        double amt = (double)(System.currentTimeMillis())/1000.0;
         gl.glClear(GL_DEPTH_BUFFER_BIT);
 
         float bkg[] = { 0.0f, 0.0f, 0.0f, 1.0f };
@@ -94,33 +108,85 @@ public class Main extends JFrame implements GLEventListener {
 
         gl.glUseProgram(rendering_program);
 
-        // build view matrix
-        Matrix3D vMat = new Matrix3D();
-        vMat.translate(-cameraX,-cameraY,-cameraZ);
-
-        // build model matrix
-        Matrix3D mMat = new Matrix3D();
-
-        int proj_loc = gl.glGetUniformLocation(rendering_program, "proj_matrix");
-        int v_loc = gl.glGetUniformLocation(rendering_program, "v_matrix");
-        int m_loc = gl.glGetUniformLocation(rendering_program, "m_matrix");
-        int t_loc = gl.glGetUniformLocation(rendering_program, "t");
-
-        gl.glUniformMatrix4fv(proj_loc, 1, false, pMat.getFloatValues(), 0);
-        gl.glUniformMatrix4fv(v_loc, 1, false, vMat.getFloatValues(), 0);
-        gl.glUniformMatrix4fv(m_loc, 1, false, mMat.getFloatValues(), 0);
-        gl.glUniform1f(t_loc, (float)t);
-
-        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-        gl.glEnableVertexAttribArray(0);
-
         gl.glEnable(GL_DEPTH_TEST);
         gl.glDepthFunc(GL_LEQUAL);
-
         //gl.glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-        gl.glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100000);
+        // build view matrix
+        scene.pushMatrix();
+        /*scene.rotate(System.currentTimeMillis() / 10.0 % 10,
+                1.0,0.0,0.0);*/
+        scene.translate(-cameraX, -cameraY, -cameraZ);
+
+        // build sun's MV matrix
+        scene.pushMatrix();
+        scene.translate(sunX, sunY, sunZ);
+
+        // add additional matrix for sun's rotation
+        scene.pushMatrix();
+        scene.rotate(System.currentTimeMillis() / 10.0,
+                1.0,1.0,1.0);
+        scene.scale(1.25, 1.25, 1.25);
+
+        gl.glUniformMatrix4fv(proj_loc, 1, false,
+                pMat.getFloatValues(), 0);
+        gl.glUniformMatrix4fv(mv_loc, 1, false,
+                scene.peek().getFloatValues(), 0);
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        gl.glVertexAttribPointer(0, 3, GL_FLOAT,
+                false, 0, 0);
+        gl.glEnableVertexAttribArray(0);
+        gl.glDrawArrays(GL_TRIANGLES, 0, 18);
+        // remove sun's rotation
+        scene.popMatrix();
+        // build earth's mv
+        scene.pushMatrix();
+        scene.translate(Math.cos(amt)*10.0f, 0.0f, Math.sin(amt)*10.0f);
+        //scene.translate(earthX, earthY, earthZ);
+
+        // earth's rotation
+        scene.pushMatrix();
+        scene.rotate(System.currentTimeMillis() / 10.0,
+                1.0,1.0,1.0);
+        scene.scale(0.75, 0.75, 0.75);
+
+        gl.glUniformMatrix4fv(proj_loc, 1,
+                false, pMat.getFloatValues(), 0);
+        gl.glUniformMatrix4fv(mv_loc, 1,
+                false, scene.peek().getFloatValues(), 0);
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+        gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        gl.glEnableVertexAttribArray(0);
+        gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+        // remove earth's rotation
+        scene.popMatrix();
+
+        // build moon's mv
+        scene.pushMatrix();
+        scene.translate(0.0f, Math.sin(amt)*2.0f, Math.cos(amt)*2.0f);
+        //scene.translate(moonX, moonY, moonZ);
+        scene.rotate(System.currentTimeMillis() / 10.0,
+                1.0,1.0,1.0);
+
+        scene.scale(0.25, 0.25, 0.25);
+
+        gl.glUniformMatrix4fv(proj_loc, 1,
+                false, pMat.getFloatValues(), 0);
+        gl.glUniformMatrix4fv(mv_loc, 1,
+                false, scene.peek().getFloatValues(), 0);
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+        gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        gl.glEnableVertexAttribArray(0);
+        gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // remove moon's mv
+        scene.popMatrix();
+
+        // remove earth's, sun's, view's translations
+        scene.popMatrix(); scene.popMatrix(); scene.popMatrix();
     }
 
     public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
@@ -136,7 +202,7 @@ public class Main extends JFrame implements GLEventListener {
         String fshaderSource[] = readShaderSource("shaders/frag.shader");
 
         int vShader = gl.glCreateShader(GL_VERTEX_SHADER);
-        gl.glShaderSource(vShader, 69, vshaderSource, null, 0); // note: 3 lines of code
+        gl.glShaderSource(vShader, 12, vshaderSource, null, 0); // note: 3 lines of code
         gl.glCompileShader(vShader);
 
         checkOpenGLError();
@@ -197,7 +263,17 @@ public class Main extends JFrame implements GLEventListener {
 
     private void setupVertices() {
         GL4 gl = (GL4) GLContext.getCurrentGL();
-        float[ ] vertex_positions =
+
+        float[] pyramid_positions =
+                {
+                        -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+                        1.0f, -1.0f, 1.0f,1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+                        1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+                        -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+                        -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f,
+                        1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f
+                };
+        float[] cube_positions =
                 {
                         -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f,
                         -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,
@@ -216,9 +292,15 @@ public class Main extends JFrame implements GLEventListener {
         gl.glGenVertexArrays(vao.length, vao, 0);
         gl.glBindVertexArray(vao[0]);
         gl.glGenBuffers(vbo.length, vbo, 0);
+
         gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(vertex_positions);
-        gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf,
+        FloatBuffer pyramidBuf = Buffers.newDirectFloatBuffer(pyramid_positions);
+        gl.glBufferData(GL_ARRAY_BUFFER, pyramidBuf.limit()*4, pyramidBuf,
+                GL_STATIC_DRAW);
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+        FloatBuffer cubeBuf = Buffers.newDirectFloatBuffer(cube_positions);
+        gl.glBufferData(GL_ARRAY_BUFFER, cubeBuf.limit()*4, cubeBuf,
                 GL_STATIC_DRAW);
     }
 }
